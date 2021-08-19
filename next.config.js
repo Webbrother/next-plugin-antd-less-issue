346f0703576f73821615605f3a87ecf3e9081a43
webbrother <komarov.vitaliy@gmail.com>
2021-08-19 15:19:53 +0400

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-}
+// module.exports = {
+//   reactStrictMode: true,
+// }
+
+const withAntdLess = require('next-plugin-antd-less');
+
+module.exports = withAntdLess({
+  // optional
+  // modifyVars: {
+  //   '@primary-color': 'red',
+  //   '@tabs-title-font-size': '18px'
+  // },
+
+  // optional
+  lessVarsFilePath: './styles/variables.less',
+
+  // optional
+  lessVarsFilePathAppendToEndOfContent: false,
+  // optional https://github.com/webpack-contrib/css-loader#object
+
+  cssLoaderOptions: {},
+
+  webpack(config) {
+    return config;
+  },
+
+});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,10 @@
+import 'antd/dist/antd.css'
+
+// import 'antd/lib/style'
+// import 'antd/lib/button/style'
+// import '../styles/ant-button.less'
+
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
